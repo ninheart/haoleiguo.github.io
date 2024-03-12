7ddc80755543cf87ee27ffc7562b7e4a0e37bbb7
Charlie <charlieliu1016@gmail.com>
2024-03-11 20:53:49 -0400

    set up for headers

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BsFillMoonStarsFill } from 'react-icons/bs'
+import '../styles/Header.css';
+
+const Header = () => {
+    return (
+      <div className='header-container'>
+        <div className='text-container'>
+          <h1>{'Hello'}</h1>
+        </div>
+        <div className='darkmode-container'>
+          <BsFillMoonStarsFill />
+        </div>
+      </div>
+    )
+}
+
+export default Header;
