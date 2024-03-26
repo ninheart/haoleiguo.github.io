7c63e3bdf2a3337000fbeb68abdc995fc73d6508
Charlie <charlieliu1016@gmail.com>
2024-03-26 09:03:45 -0400

    fix

--- src/components/Content.js
+++ src/components/Content.js
@@ -130,7 +130,7 @@ const parsedExperience = JSON.parse(storedExperiences)
 const Content = ({isLoggedIn}) => {
 
   const [isFormOpen, setIsFormOpen] = useState(false)
-  const [experiences, setExperiences] = useState(parsedExperience)
+  const [experiences, setExperiences] = useState([])
   const [selectedExperienceIndex, setSelectedExperienceIndex] = useState(null)
   const {darkMode} = useTheme();
 
